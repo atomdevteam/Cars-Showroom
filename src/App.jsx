e404c663c47050983a8f4f87c13761fd7adc9890
jose22gonzalez <urenajosealbeto@gmail.com>
2024-05-05 00:18:19 -0400

    CS-14-Product-Tesla-Model-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import ProductTeslaLayout from './Layout/ProductTeslaLayout/ProductTeslaLayout'
+import Footer from './Layout/Footer/Footer'
 
 function App() {
 
 
   return (
     <>
-
+      <div className=''>
+        <ProductTeslaLayout />
+      </div>
+      <Footer />
     </>
   )
 }
