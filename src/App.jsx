2cfedd2e993204e57d3b521a91f3c022f2a84387
jose22gonzalez <urenajosealbeto@gmail.com>
2024-05-05 00:22:38 -0400

    CS-17-Contact-Us

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
+import ContactLayout from './Layout/ContactLayout/ContactLayout'
+import Footer from './Layout/Footer/Footer'
 
 function App() {
 
-
   return (
     <>
-
+      <div className=''>
+        <ContactLayout />
+      </div>
+      <Footer />
     </>
+
   )
 }
 
 export default App
+
---
+++ src/Layout/ContactLayout/ContactLayout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Contact from '../../Components/Contact/Contact'
+import Location from '../../Components/Contact/Location'
+import Navbar from '../NavBar/NavBar'
+
+const ContactLayout = () => {
+  return (
+    <>
+      <Navbar background={'dark:bg-[#12232E]'}/>
+      <div className='bg-[#0B0C10] max-h-full '>
+
+
+        <div className='flex flex-col ml-[6rem] mr-[6rem] '>
+          <Contact />
+          <Location />
+        </div>
+      </div>  </>
+  )
+}
+
+export default ContactLayout
