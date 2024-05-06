c022e18583a66f160a481f26a5bbd8d619a6c51f
StormySilverio05 <stormysilverionunez@gmail.com>
2024-05-05 22:34:47 -0400

    CS-5-New-and-Used-cars, New Car Page finihed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import PrincipalSection from './assets/components/PrincipalSection'
+import Footer from './assets/components/Footer'
+import Header from './assets/components/Header'
 
 function App() {
 
 
   return (
     <>
-
+      <Header/>
+      <PrincipalSection/>
+      <Footer/>
     </>
   )
 }
