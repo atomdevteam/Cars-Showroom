65bc28c40d00e6ad8e4703d0e75090077d592cfd
Julio <jimeno.juliocesar@gmail.com>
2024-05-07 21:39:38 -0400

    CS-9-Product-Tesla Model-3-SRP

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
+import Footer from './Layout/Footer/Footer'
+import Navbar from './Layout/NavBar/NavBar'
+import ProductDetails from './Components/ProductDetails/ProductDetalle/ProductoDetalle'
 
 function App() {
 
 
   return (
     <>
+    <div className=''>
+      <Navbar/>
 
+      <ProductDetails/>
+
+    </div>
+
+    <Footer/>
     </>
   )
 }
--- src/Layout/NavBar/NavBar.jsx
+++ src/Layout/NavBar/NavBar.jsx
@@ -3,13 +3,13 @@ const Navbar = ({background}) => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
 
-        <nav className={`bg-white border-gray-200 ${background}`}>
-            <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
+        <nav className={`bg-[#12232E] border-gray-200 ${background}`}>
+            <div className=" bg-[#12232E] flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
                 <a href="https://flowbite.com" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="https://i.ibb.co/xXWCwHF/logo.png" className="h-12" alt="logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Cars  Showroom</span>
                 </a>
-                <div className="flex items-center md:order-2 space-x-1 md:space-x-2 rtl:space-x-reverse">
+                <div className="bg-[#12232E]  flex items-center md:order-2 space-x-1 md:space-x-2 rtl:space-x-reverse ">
                     <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Sign in</a>
                     {/* <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign up</a> */}
                     <button data-collapse-toggle="mega-menu" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu" aria-expanded="false">
@@ -20,7 +20,7 @@ const Navbar = ({background}) => {
                     </button>
                 </div>
 
-                <div id="mega-menu" className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+                <div id="mega-menu" className=" items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
                     <ul className="flex flex-col mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse">
                         <li>
                             <a href="#" className="block py-2 px-3 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-blue-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
