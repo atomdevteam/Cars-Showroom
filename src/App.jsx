253e1a0a79f96dc73ae010c0a01680a9dc18338d
Julio <jimeno.juliocesar@gmail.com>
2024-05-10 17:49:29 -0400

    CS-23-News

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
+import Footer from './Layout/Footer/Footer'
+import Navbar from './Layout/NavBar/NavBar'
+import News from './Components/News/New'
 
 function App() {
 
 
   return (
     <>
+    <div className=''>
+      <Navbar/>
 
+      <News/>
+    </div>
+
+    <Footer/>
     </>
   )
 }
---
+++ src/Components/UltimaNoticias/UltimasNoticias.jsx
@@ -0,0 +1,9 @@
+
+const UltimaNoticias =() => {
+    return(
+        <div className="text-4xl font-bold text-zinc-300 max-md:max-w-full">
+            <P>Última Noticias</P>
+    </div>
+    );
+}
+export default UltimaNoticias
