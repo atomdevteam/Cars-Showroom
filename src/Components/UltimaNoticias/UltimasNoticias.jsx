82fc8ad17bd4078e4d66fc55e9850022027b9e07
Julio <jimeno.juliocesar@gmail.com>
2024-05-14 16:11:58 -0400

    CS-12-News

--- src/Components/UltimaNoticias/UltimasNoticias.jsx
+++ src/Components/UltimaNoticias/UltimasNoticias.jsx
@@ -2,7 +2,7 @@
 const UltimaNoticias =() => {
     return(
         <div className="text-4xl font-bold text-zinc-300 max-md:max-w-full">
-            <P>Última Noticias</P>
+            <P>Última Noticias</P>2
     </div>
     );
 }
