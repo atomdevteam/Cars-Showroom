2fb10a9461de2b0ed46f45eb68a61440122ec36d
jose22gonzalez <urenajosealbeto@gmail.com>
2024-05-17 14:05:43 -0400

    CS-5-User-Car

--- src/Layout/NavBar/NavBar.jsx
+++ src/Layout/NavBar/NavBar.jsx
@@ -26,7 +26,7 @@ const Navbar = ({ background }) => {
                 <div id="mega-menu" className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
                     <ul className="flex flex-col mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse">
                         <li>
-                            <a href="/" className="block py-2 px-3 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-blue-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
+                            <a href="#/" className="block py-2 px-3 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-blue-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
                         </li>
 
                         <li>
--- src/Layout/UsedCars/UsedCars.jsx
+++ src/Layout/UsedCars/UsedCars.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
 import Principal from './components/Principal'
-import Footer from './components/Footer'
+
 
 function UsedCars() {
   return (
     <div>
         <Header/>
         <Principal/>
-        <Footer/>
     </div>
   )
 }
