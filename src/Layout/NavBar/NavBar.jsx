8ca27c983439c12ce51bacc1271bba319c1eb3a3
jose22gonzalez <urenajosealbeto@gmail.com>
2024-05-17 16:07:45 -0400

    CS-20

--- src/Layout/NavBar/NavBar.jsx
+++ src/Layout/NavBar/NavBar.jsx
@@ -57,7 +57,7 @@ const Navbar = ({background}) => {
                                                 </a>
                                             </li>
                                             <li>
-                                                <a href="#" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                                <a href="#/CarInspection" className="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
                                                     Revisión de coches
                                                 </a>
                                             </li>
