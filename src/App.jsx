6989b75b3ddd96612111cd6f15e7ad2a45c413c4
jose22gonzalez <urenajosealbeto@gmail.com>
2024-05-17 12:08:18 -0400

    CS-14

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { HashRouter as Router, Route, Routes, Navigate, useNavigate } from 'react-router-dom'
 import CarSale from './Layout/CarSale/CarSaleLayout'
 import BuyCar from './Layout/BuyCarLayout/BuyCarLayout'
+import ProductTeslaModel from './Layout/ProductTeslaLayout/ProductTeslaLayout'
 import Footer from './Layout/Footer/Footer'
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/CarSale' element={<CarSale />} />
           <Route path='/BuyCar' element={<BuyCar />} />
+          <Route path='/ProductTeslaModel' element={<ProductTeslaModel />} />
         </Routes>
       </Router>
       <Footer />
