f78e17578fde08506f1d554417386e12ef9e9eae
Julio <jimeno.juliocesar@gmail.com>
2024-06-11 16:04:29 -0400

    CS-45-Delete-Components-Main-Screen

--- src/Layout/MainScreen/MainScreen.jsx
+++ src/Layout/MainScreen/MainScreen.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import Navbar from "../NavBar/NavBar"
 import Hero from '../../Components/Hero/Hero'
-import Recomendado from '../../Components/Recomendado/Recomendado'
 import Testimonio from '../../Components/Testimono/Testimonio'
 import Contacto from '../../Components/Contacto/Contacto'
 const MainScreen = () => {
